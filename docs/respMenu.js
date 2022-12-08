f4d250f503392cd102dc9b4f2e8fe0fe1b768115
Yuying-Jin <yxj5181@psu.edu>
2022-12-08 11:37:42 -0500

    add animation for sidebar

--- docs/respMenu.js
+++ docs/respMenu.js
@@ -4,18 +4,16 @@ function listen() {
     var hamburger = document.getElementById("hamburger")
     hamburger.addEventListener('click', openMe, false)
     hamburger.addEventListener('touch', openMe, false)
-    var closeX =  document.getElementById("closeMe")
+    var closeX = document.getElementById("closeMe")
     closeX.addEventListener('click', hideMe, false)
     closeX.addEventListener('touch', hideMe, false)
-   
     
 }
 
 function openMe() {
-    document.getElementsByClassName("sidebar")[0].style.display = "block";
-    
+    document.getElementsByClassName("sidebar")[0].classList.add('openSidebar')
+
 }
 function hideMe() {
-    document.getElementsByClassName("sidebar")[0].style.display = "none";
-    
+    document.getElementsByClassName("sidebar")[0].classList.remove('openSidebar')
 }
